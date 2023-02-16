24cf172c59ac22eecf452cff41625145e0a64a44
emily <emily@emily-HP-Pavilion-Notebook>
2023-02-16 12:59:05 -0500

    done

--- src/Kingdomino.java
+++ src/Kingdomino.java
@@ -3,5 +3,7 @@ public class Kingdomino {
     public static void main(String[] args){
         System.out.println("Salutation!");
         System.out.println("merged");
+        System.out.println("Salutationsss!");
+
     }
 }
