db8abea423e72d2f62f4aec56c26dde2c61443f8
Jiming <71953568+Jiming-C@users.noreply.github.com>
2023-02-16 13:06:54 -0500

    minion update

--- src/Kingdomino.java
+++ src/Kingdomino.java
@@ -4,6 +4,6 @@ public class Kingdomino {
         System.out.println("Salutation!");
         System.out.println("merged");
         System.out.println("Salutationsss!");
-
+        System.out.println("I love Minions")
     }
 }
