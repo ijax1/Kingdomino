064bc05efc51980873ff43c2c4dcdaddf8dd819f
Jiming <71953568+Jiming-C@users.noreply.github.com>
2023-02-17 13:02:28 -0500

    initial methods

---
+++ src/Backend/Deck.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+class deck() {
+    private ArrayList<Domino> deck;
+    private Domino[] dominoesToSelect;
+    private boolean[] selected;
+
+    public boolean (boolean isTwoPlayer) {
+        
+    }
+
+    public shuffleDeck() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public Domino[] getDominoes() {
+
+    }
+
+    public boolean chooseDomino(Player player){
+
+    }
+
+
+}
