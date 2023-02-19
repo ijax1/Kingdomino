4550a62774c8efedf76d36c391e760c7826cc7da
Jiming-C <jimingchen2015@gmail.com>
2023-02-19 01:41:52 -0500

    added methods

--- src/Backend/Player.java
+++ src/Backend/Player.java
@@ -6,17 +6,20 @@ import java.util.Random;
 public abstract class Player {
 
     //class vars
-    static final String MEDIEVAL_TITLES;
+    static final String[] MEDIEVAL_TITLES = {"The Honorable", "The Great", "The Wise", "The Bold", "The Valiant", "The Chivalrous", "The Magnificent", "The Noble", "The Righteous", "The Sagacious"};
 
     //instance
-    String name;
-    int score;
-    Color color;
-    Grid grid;
-    Domino currentDomino;
-    Domino nextDomino;
+    private String name;
+    private int score;
+    private Color color;
+    private Grid grid;
+    private Domino currentDomino;
+    private Domino nextDomino;
 
     //methods
+
+    abstract boolean isHuman();
+
     public Player(Color color, String name, String title) {
         this.color = color;
         this.name = name;
@@ -35,9 +38,32 @@ public abstract class Player {
     }
 
     public String generateTitles() {
-        Random rand = new Random();
+        // Random rand = new Random();
+        // int randIndex = rand.nextInt(MEDIEVAL_TITLES.length);
+        // name = MEDIEVAL_TITLES[randIndex];
     }
 
+    public String generateAIName() {
 
+    }
 
+    public Grid getGrid() {
+        return grid;
+    }
+
+    public void placeDomino(int x, int y, Domino domino) {
+        if (grid.avilableSpaces(domino)) {
+            grid.placeDomino(x, y, domino);
+        } else {
+            //prompt user?
+        }
+    }
+
+    public void selecteDomino(Domino domino) {
+
+    }
+
+    public String generateTitle() {
+
+    }
 }
