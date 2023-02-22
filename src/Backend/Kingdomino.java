87b4df5072f84994787ab8bcb80ab0074320189d
emily <emily@emily-HP-Pavilion-Notebook>
2023-02-22 12:25:22 -0500

    done

--- src/Backend/Kingdomino.java
+++ src/Backend/Kingdomino.java
@@ -6,5 +6,7 @@ public class Kingdomino {
         System.out.println("merged");
         System.out.println("Salutationsss!");
         System.out.println("I love Minions");
+        System.out.println("merged 2");
+
     }
 }
