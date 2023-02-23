fcbad610e8a50e7966c8f4a178a945f6d7a6c29d
Jiming-C <jimingchen2015@gmail.com>
2023-02-23 16:40:29 -0500

    good changes

--- src/Backend/Player.java
+++ src/Backend/Player.java
@@ -1,55 +1,80 @@
 package Backend;
+
 import java.awt.Color;
 import java.util.Random;
+
 public abstract class Player {
-    //class vars
+    // Class variables
     static final String[] MEDIEVAL_TITLES = {"The Honorable", "The Great", "The Wise", "The Bold", "The Valiant", "The Chivalrous", "The Magnificent", "The Noble", "The Righteous", "The Sagacious"};
-    //instance
+
+    // Instance variables
     private String name;
     private int score;
     private Color color;
     private Grid grid;
     private Domino currentDomino;
     private Domino nextDomino;
-    //methods
-    abstract boolean isHuman();
+
+    // Constructor
     public Player(Color color, String name, String title) {
         this.color = color;
         this.name = name;
         this.score = 0;
-        //create empty grid
+        // Create empty grid
         this.grid = new Grid(title);
     }
+
+    // Abstract method to check if player is human
+    abstract boolean isHuman();
+
+    // Getters for instance variables
+    public int getScore() {
+        return score;
+    }
+
     public Color getColor() {
         return color;
     }
+
     public String getName() {
         return name;
     }
 
-    public String generateTitle() {
-        Random rand = new Random();
-        int randIndex = rand.nextInt(MEDIEVAL_TITLES.length);
-        //chooses a random title in MEDIEVAL_TITLES class variable
-        return MEDIEVAL_TITLES[randIndex];
-    }
-    public String generateAIName() {
-        //returns a temp name for the AI
-        return "Computer";
-    }
     public Grid getGrid() {
         return grid;
     }
-    public void placeDomino(int x, int y, Domino domino) {
-        if (grid.avilableSpaces(domino)) {
-            grid.placeDomino(x, y, domino);
-        } else {
-            //doesn't go through
-            //activate message box
-        }
+
+    public Domino getCurrentDomino() {
+        return currentDomino;
     }
 
-    public void selecteDomino(Domino domino) {
+    public Domino getNextDomino() {
+        return nextDomino;
+    }
+
+    // Generates a random medieval title
+    public String generateTitle() {
+        Random rand = new Random();
+        int randIndex = rand.nextInt(MEDIEVAL_TITLES.length);
+        return MEDIEVAL_TITLES[randIndex];
+    }
+
+    // Returns a temporary name for the AI
+    public String generateAIName() {
+        return "Computer";
+    }
+
+    // Places a domino on the grid at the given coordinates
+    public void placeDomino(int x, int y, Domino domino) {
+        if (grid.availableSpaces(domino)) {
+            grid.placeDomino(x, y, domino);
+        } else {
+            // Activate message box
+        }
+    }
+    
+    // Sets the next domino to be played
+    public void selectDomino(Domino domino) {
         this.nextDomino = domino;
     }
 
