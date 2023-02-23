bb9303687661ebec3ed4047f2055612cc9df5522
Jonathan Jayaputra <91294639+jthanoan@users.noreply.github.com>
2023-02-23 12:33:03 -0500

    start on grid

---
+++ src/UIComponents/UIGrid.java
@@ -0,0 +1,13 @@
+package UIComponents;
+import UIComponents.Render.*;
+
+public class UIGrid {
+
+    public UIGrid(Coordinate center, Grid g){
+
+    }
+
+    public void addDominoToGrid(){
+
+    }
+}
