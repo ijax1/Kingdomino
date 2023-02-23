9dd80d152d61ccc615770796f32f040e708380ce
Jiming <71953568+Jiming-C@users.noreply.github.com>
2023-02-23 12:48:46 -0500

    update

--- src/Backend/Deck.java
+++ src/Backend/Deck.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 class deck() {
     private ArrayList<Domino> deck;
     private Domino[] dominoesToSelect;
-    private boolean[] selected;
+    private boolean[] selected = {false, false, false, false};
 
     public deck(boolean isTwoPlayer) {
         //create a deck of random dominos
@@ -18,12 +18,30 @@ class deck() {
         collections.shuffle(deck);
     }
 
+    public int dominoesRemaining() {
+        return deck.size();
+    }
+
     public boolean isEmpty() {
         return deck.isEmpty();
     }
 
     public Domino[] getDominoesToSelect() {
+        //what about 3 players?
         //remove 4 items from main deck and place them in list
-        dominoesToSelect = [deck.remove(0), deck.remove(0), deck.remove(0), deck.remove(0)];
+        if (this.deminoesRemaining()) {
+            dominoesToSelect = [deck.remove(0), deck.remove(0), deck.remove(0), deck.remove(0)];
+        } else {
+            //not enough cards
+        }
+        
+    }
+
+    public void setSelected(int index) {
+        selected[index] = true;
+    }
+
+    public boolean[] getSelected() {
+        return selected;
     }
 }
