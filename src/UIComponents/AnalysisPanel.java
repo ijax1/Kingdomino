83437870aa3c395f03a9a9174a4ece14eb39a7cf
Jiming <71953568+Jiming-C@users.noreply.github.com>
2023-02-28 12:38:48 -0500

    WIP

--- src/UIComponents/AnalysisPanel.java
+++ src/UIComponents/AnalysisPanel.java
@@ -1,84 +1,130 @@
 package UIComponents;
 
+import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
-import java.util.ArrayList;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 
-import Backend.Kingdomino;
-import Backend.Player;
 import UIComponents.Render.Coordinate;
 import resources.OurColors;
 import resources.Resources;
-class Dummy extends Component {
-	Dummy(Coordinate position, Kingdomino k) {
-		super(position, k);
-		// TODO Auto-generated constructor stub
-	}
 
-	@Override
-	public void setPosition(Coordinate coordinate) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public boolean onComponent(Coordinate c) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void draw(Graphics2D g) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void whenClicked() {
-		// TODO Auto-generated method stub
-		
-	}};
-public class AnalysisPanel extends JPanel implements MouseListener, MouseMotionListener, KeyListener {
-	private static final long serialVersionUID = 7381080659172927952L;
-	private ArrayList<Component>components = new ArrayList<Component>();
-	private Font medieval;
-	private Font medievalLg;
-	private int mousedx;
-	private int mousedy;
-	private UIDomino domino;
-	private int mousex, mousey;
-	private PlayerTabGroup group;
-	private PlayerTabButton button;
+@SuppressWarnings("serial")
+public class AnalysisPanel extends JPanel {
+	BufferedImage player;
+	BufferedImage computer;
+	BufferedImage none;
+	CloseButton close;
 	
-	public AnalysisPanel(ArrayList<Player> players, Kingdomino k) {
-		setPreferredSize(new Dimension(1280,720));
-		setOpaque(true);
-		setBackground(new Color(100,100,100));
-		addMouseListener(this);
-		addMouseMotionListener(this);
-		addKeyListener(this);
-		medieval = Resources.loadFont("fonts/MedievalSharp-Regular.ttf", 100);
-		group = new PlayerTabGroup(players,k);
-		//button = new PlayerTabButton(new Coordinate(0,160,0), k, new Player());
+	public AnalysisPanel(GridBagLayout g) {
+		super(g);
+		//setBackground(OurColors.BACKGROUND);
+		GridBagConstraints c = new GridBagConstraints();
 		
-		components.add(group);
-		//components.add(new Hitbo)
+		PlayerSelectPanel player1 = new PlayerSelectPanel(OurColors.RED, 1, PlayerSelectPanel.HUMAN);
+		PlayerSelectPanel player2 = new PlayerSelectPanel(OurColors.BLUE, 2, PlayerSelectPanel.COMPUTER);
+		PlayerSelectPanel player3 = new PlayerSelectPanel(OurColors.GREEN, 3, PlayerSelectPanel.COMPUTER);
+		PlayerSelectPanel player4 = new PlayerSelectPanel(OurColors.YELLOW, 4, PlayerSelectPanel.COMPUTER);
+		//JButton scrollB = new JButton("Quiteth");
+		JLabel scroll = new JLabel("Kingdomino", SwingConstants.CENTER);
+		scroll.setFont(Resources.getMedievalFont(50));
+		scroll.setForeground(OurColors.FONT_LIGHT);
+		RoyalButton exit = new RoyalButton("Exiteth");
+		RoyalButton play = new RoyalButton("Playeth");
+		exit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}	
+		});
+		play.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				/*
+				changePanel(GameState.PLAYER_TURN);
+				 */
+			}	
+		});
+		
+		//close = new CloseButton(new Coordinate(1200,800,0), null);	
+		
+		//Settings for whole layout 
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx=0.5;
+		c.gridheight = 1;
+		c.insets = new Insets(10,10,10,10);
+		c.anchor = GridBagConstraints.CENTER;
+		
+		//Settings for individual components
+		c.gridwidth = 4;
+		c.weighty = 0;	//Title can get cut off
+		c.gridx = 0;
+		c.gridy = 0;
+		//c.gridwidth = GridBagConstraints.REMAINDER;
+		g.setConstraints(scroll, c);
+		add(scroll);
+		//c.weighty = 0.2;
+		c.gridwidth = 1;
+		c.gridx = 0;
+		c.gridy = 1;
+		g.setConstraints(player1, c);
+		add(player1);
+		c.gridx = 1;
+		c.gridy = 1;
+		g.setConstraints(player2, c);
+		add(player2);
+		c.gridx = 2;
+		c.gridy = 1;
+		g.setConstraints(player3, c);
+		add(player3);
+		c.gridx = 3;
+		c.gridy = 1;
+		g.setConstraints(player4, c);
+		add(player4);
+		c.ipady = 20;
+		c.gridx = 0;
+		c.gridy = 2;
+		c.gridwidth = 2;
+		g.setConstraints(exit, c);
+		add(exit);
+		
+		c.gridx = 2;
+		c.gridy = 2;
+		c.gridwidth = 2;
+		g.setConstraints(play, c);
+		add(play);
+		player = Resources.loadImage("player_icon.png");
+		computer = Resources.loadImage("computer_icon.png");
+		none = Resources.loadImage("none_icon.png");
 	}
+	@Override
 	public void paintComponent(Graphics g1) {
-		Graphics2D g = (Graphics2D) g1;
-		
-		applyHints(g);
+		Graphics2D g = (Graphics2D)g1;
+		GamePanel.applyHints(g);
+
 		g.setColor(OurColors.BACKGROUND);
 		g.fillRect(0, 0, getWidth(), getHeight());
 		g.setColor(OurColors.BACKGROUND_CIRCLE);
@@ -89,95 +135,11 @@ public class AnalysisPanel extends JPanel implements MouseListener, MouseMotionL
 		//player = tint(player, new Color(0xe06666));
 		//g.drawImage(player, 100,100,200,200, null);
 		//g.drawImage(player, 100,100,100,100,null);
-		if(medieval==null)System.out.println("true");
-		g.setFont(medievalLg);
-//		g.drawString("Hello world", 200,200);
-//		g.fillOval(500, 500, 10, 10);
-//		g.drawRect(480, 480, 40, 40);
-		for(Component component: components) {
-			//TODO: currently, every component uses the same graphics object. Is this ok?
-			//We may need to copy the graphics object using g.create() or g.copyarea()
-			component.draw((Graphics2D) g.create());
-		}
 	}
-    public static void applyHints(Graphics2D g2d) {
-        g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
-        g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_DISABLE);
-        g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
-        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-        g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
-        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-    }
-    
-	private void drawBg(Graphics2D g) {
-
-	}
-	private void drawStaticTiles() {
-		
-	}
-	public void drawError(String header, String message) {
-		
-	}
-
-	//Mouse, Key Event Handlers
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		int x = e.getX();
-		int y = e.getY();
-		
-		//TODO: this won't work, just a placeholder.
-		Coordinate mouse = new Coordinate(x,y,0);
-		
-		for(Component component: components) {
-			if (component instanceof Button){
-				//this won't work either, just a placeholder
-				if(component.getPosition().equals(mouse)) {
-				component.whenClicked();
-				}
-			}
-		}
-	}
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		mousex = e.getX();
-		mousey = e.getY();
-		System.out.println("mousex: "+mousex + " mousey: "+mousey);
-	}
-	@Override
-	public void keyPressed(KeyEvent e) {
-		System.out.println(KeyEvent.getKeyText(e.getKeyCode()));
-	}
-	public void mouseEvent(boolean isClicked, boolean isDragged, boolean isScrolling) {
-		
-	}
-	public void keyEvent(int keyCode) {
-		
-	}
-	
-	
-	//Empty methods
-	@Override
-	public void keyReleased(KeyEvent e) {
-	}
-	@Override
-	public void mouseMoved(MouseEvent e) {
-	}
-	@Override
-	public void mousePressed(MouseEvent e) {	
-	}
-	@Override
-	public void mouseReleased(MouseEvent e) {
-	}
-	@Override
-	public void mouseEntered(MouseEvent e) {
-	}
-	@Override
-	public void mouseExited(MouseEvent e) {
-	}
-	@Override
-	public void keyTyped(KeyEvent e) {
+	private BufferedImage tint(BufferedImage img, Color color) {
+		Graphics2D g2 = img.createGraphics();
+		//g2.setXORMode(color);
+		//g2.fillRect(0,0,img.getWidth(), img.getHeight());
+		return img;
 	}
 }
