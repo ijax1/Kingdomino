768d1de569cc06f1d3cffc1cf61145ed49995cc2
caleb <calebshim248@gmail.com>
2023-02-28 12:55:56 -0500

    GridPosition added

---
+++ src/Backend/GridPosition.java
@@ -0,0 +1,18 @@
+package Backend;
+
+public class GridPosition {
+    private int x;
+    private int y;
+    public GridPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
