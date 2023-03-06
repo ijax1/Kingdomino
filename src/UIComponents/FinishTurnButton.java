1cd37d0e05a547b0a072e757f3f1baf6daff6572
ayhacko <ayhuko2005@gmail.com>
2023-03-06 12:40:36 -0500

    setCurrentPlayer became updateCurrentPlayer

--- src/UIComponents/FinishTurnButton.java
+++ src/UIComponents/FinishTurnButton.java
@@ -19,7 +19,7 @@ public class FinishTurnButton extends Button{
         // need access to get maanger through kingDomnio
         // need access to set currentPlayer int in gameManager
     	System.out.println("Finish Turn button clicked");
-        getGame().getManager().setCurrentPlayer();
+        getGame().getManager().updateCurrentPlayer();
     }
 
     public boolean onComponent(Coordinate c) {
