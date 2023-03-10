7ebbb7e9e753b8f4c65d275f723dd645a9884837
ijax1 <ianshimjackson@gmail.com>
2023-03-10 12:44:41 -0500

    game panel mouse logic integrated

---
+++ src/Tests/BackendGameTester.java
@@ -0,0 +1,22 @@
+package Tests;
+
+import java.util.ArrayList;
+
+import Backend.GameManager;
+import Backend.GameManager.GameState;
+import Backend.Player;
+import resources.OurColors;
+
+public class BackendGameTester {
+	public static void main(String[]args) {
+		GameManager g = new GameManager(null);
+		ArrayList<Player>fakePlayers = new ArrayList<Player>(4);
+		fakePlayers.add(new TestPlayer(OurColors.RED, "Don Quixote", "The Ingenious",20));
+		fakePlayers.add(new TestPlayer(OurColors.BLUE, "King Arthur", "The Round",30));
+		fakePlayers.add(new TestPlayer(OurColors.GREEN, "Sir Gawain", "The Green",25));
+		fakePlayers.add(new TestPlayer(OurColors.YELLOW, "Ian Jackson", "The Glorious",25));
+		g.setPlayers(fakePlayers);
+		g.setGameState(GameState.PLAYER_TURN);
+		g.getCurrentPlayer().setNextDomino(g.getDominoesToSelect()[0]);
+	}
+}
