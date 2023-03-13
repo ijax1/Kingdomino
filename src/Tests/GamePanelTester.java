34810cd81804e0a3f9e88539edcfbbf6b646f173
Jonathan Jayaputra <91294639+jthanoan@users.noreply.github.com>
2023-03-13 11:15:34 -0400

    stuff has changed for the better or the worse

--- src/Tests/GamePanelTester.java
+++ src/Tests/GamePanelTester.java
@@ -4,11 +4,8 @@ import java.util.ArrayList;
 
 import javax.swing.JFrame;
 
-import Backend.GameManager;
+import Backend.*;
 import Backend.GameManager.GameState;
-import Backend.Kingdomino;
-import Backend.Player;
-import Backend.RandomStrategy;
 import UIComponents.GamePanel;
 import resources.OurColors;
 
@@ -23,8 +20,18 @@ public class GamePanelTester {
 		fakePlayers.add(new TestPlayer(OurColors.BLUE, "King Arthur", "The Round",30));
 		fakePlayers.add(new TestPlayer(OurColors.GREEN, "Sir Gawain", "The Green",25));
 		fakePlayers.add(new TestPlayer(OurColors.YELLOW, "Ian Jackson", "The Glorious",25));
-		
-		TestKingdomino k = new TestKingdomino();
+		Domino[] dList = {
+				new Domino(new Tile(Tile.Land.FOREST, 0),new Tile(Tile.Land.FOREST, 0),0),
+				new Domino(new Tile(Tile.Land.SWAMP, 0),new Tile(Tile.Land.WHEAT, 0),0),
+				new Domino(new Tile(Tile.Land.LAKE, 0),new Tile(Tile.Land.PASTURE, 0),0),
+				new Domino(new Tile(Tile.Land.MINE, 0),new Tile(Tile.Land.LAKE, 0),0)
+		};
+		int ind = 0;
+		for(Player p: fakePlayers){
+			p.setCurrentDomino(dList[ind]);
+			ind++;
+		}
+		Kingdomino k = new Kingdomino();
 		k.getManager().setPlayers(fakePlayers);
 		k.changePanel(GameState.PLAYER_TURN);
 //		GameManager gm = k.getManager();
