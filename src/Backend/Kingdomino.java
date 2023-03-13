8228a2db1340eccd9ec186a25ef07aa94e1c58a0
caleb <calebshim248@gmail.com>
2023-03-13 12:54:24 -0400

    kingdomino handles gamestate.strategy

--- src/Backend/Kingdomino.java
+++ src/Backend/Kingdomino.java
@@ -14,6 +14,7 @@ import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 
 import Backend.GameManager.GameState;
+import UIComponents.AnalysisPanel;
 import UIComponents.GamePanel;
 import UIComponents.PodiumPanel;
 import UIComponents.StartPanel;
@@ -21,11 +22,12 @@ import resources.Resources;
 
 
 public class Kingdomino {
-    
+
     final private JPanel basePanel;
     final private StartPanel startPanel;
     private GamePanel gamePanel;
     final private PodiumPanel podiumPanel;
+    final private AnalysisPanel analysisPanel;
     final private GameManager manager;
     final private CardLayout panels = new CardLayout();
     private JFrame frame;
@@ -35,13 +37,12 @@ public class Kingdomino {
 
     public Kingdomino() {
         manager = new GameManager(this);
-        GridBagLayout gb = new GridBagLayout();
         basePanel = new JPanel(panels);
-        
-        startPanel = new StartPanel(gb, this);
-//        gamePanel = new GamePanel(this);
+
+        startPanel = new StartPanel(new GridBagLayout(), this);
         podiumPanel = new PodiumPanel(new GridBagLayout(), this);
-        
+        analysisPanel = new AnalysisPanel(new GridBagLayout());
+
         basePanel.add(startPanel, "Start Panel");
 //        basePanel.add(gamePanel, "Game Panel");
         basePanel.add(podiumPanel, "Podium Panel");
@@ -57,7 +58,7 @@ public class Kingdomino {
     private void setUpFrame() {
         frame = new JFrame("Kingdomino");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
 //        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setContentPane(basePanel);
 //        frame.setPreferredSize(screenSize);
@@ -66,40 +67,43 @@ public class Kingdomino {
         frame.pack();
         //frame.setResizable(false);
         frame.setVisible(true);
-        
+
         frame.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
                 KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "Cancel"); //$NON-NLS-1$
-        frame.getRootPane().getActionMap().put("Cancel", new AbstractAction()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
+        frame.getRootPane().getActionMap().put("Cancel", new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
                 System.exit(0);
                 //framename.setVisible(false);
             }
         });
     }
+
     public JFrame getFrame() {
-    	return frame;
+        return frame;
     }
 
-    public GameManager getManager() {return manager;}
+    public GameManager getManager() {
+        return manager;
+    }
 
     public void changePanel(GameState state) {
 
         if (state == GameState.INITIAL) {
             panels.show(basePanel, "Start Panel");
-        } else if (state == GameState.PLAYER_TURN || 
-        		state == GameState.TALLY_SCORE) {
-        	if(gamePanel == null) {
-        		initializeGamePanel();
-        	}
-        	panels.show(basePanel, "Game Panel");
+        } else if (state == GameState.PLAYER_TURN ||
+                state == GameState.TALLY_SCORE) {
+            if (gamePanel == null) {
+                initializeGamePanel();
+            }
+            panels.show(basePanel, "Game Panel");
         } else if (state == GameState.ENDSCREEN) {
-        	panels.show(basePanel, "Podium Panel");
+            panels.show(basePanel, "Podium Panel");
+        } else if (state == GameState.STRATEGY) {
+            panels.show(basePanel, "Strategy Analysis Panel");
         } else {
             //keep panel
         }
-        
+
     }
 
     public GamePanel getGamePanel() {
@@ -107,13 +111,12 @@ public class Kingdomino {
     }
 
 
-
     public static void main(String[] args) {
-    	SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				new Kingdomino();
-			}
-    	});
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Kingdomino();
+            }
+        });
     }
 }
